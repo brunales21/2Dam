752e8e43c44e10a3c471f40e5deb3c0d9d7c32e7
unknown <bemimarket@gmail.com>
2023-10-17 13:55:06 +0200

    py

--- prog-serv-procesos/1trim/ProgramacionMultiproceso/src/procesoPython/Main.java
+++ prog-serv-procesos/1trim/ProgramacionMultiproceso/src/procesoPython/Main.java
@@ -6,31 +6,22 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) {
+        String archivoPy = "C:\\Users\\2DAM\\PycharmProjects\\pythonProject\\main.py";
+        ProcessBuilder pb = new ProcessBuilder("python", archivoPy);
         try {
-            // Especifica el comando para ejecutar el script Python
-            String comandoPython = "python3 /ruta/a/tu_script.py"; // Asegúrate de especificar la ruta correcta
-
-            // Crea un proceso para ejecutar el comando
-            Process proceso = Runtime.getRuntime().exec(comandoPython);
-
-            // Captura la salida del proceso (STDOUT)
-            BufferedReader reader = new BufferedReader(new InputStreamReader(proceso.getInputStream()));
+            Process p = pb.start();
+            BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
             String linea;
-
-            // Lee y muestra la salida del proceso línea por línea
-            while ((linea = reader.readLine()) != null) {
+            while ((linea = in.readLine()) != null) {
                 System.out.println(linea);
             }
-
-            // Espera a que el proceso termine
-            proceso.waitFor();
-
-            // Imprime el código de salida del proceso
-            int codigoSalida = proceso.exitValue();
-            System.out.println("El proceso Python ha finalizado con código de salida: " + codigoSalida);
-
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
+            System.out.println("Codigo de salida: "+p.waitFor());
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
+
+
     }
 }
