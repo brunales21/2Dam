929a7abd6bfcd2b19d08e6ccef9aff319598dc6f
unknown <bemimarket@gmail.com>
2023-10-18 13:59:52 +0200

    disp mov y interfaz calculadora

--- dis-interfaces-web/PruebaCalculadora/src/main/java/org/example/Main.java
+++ dis-interfaces-web/PruebaCalculadora/src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ import net.objecthunter.exp4j.ExpressionBuilder;
 public class Main {
     public static void main(String[] args) {
         // Expresión matemática en formato de cadena
-        String expression = "2*3+10-16/8+5.5";
+        String expression = "2* -3+10-16/8+5.5";
 
         // Crear un objeto ExpressionBuilder para construir la expresión
         ExpressionBuilder builder = new ExpressionBuilder(expression);
--- dis-interfaces-web/calculadora/src/Calculadora.java
+++ dis-interfaces-web/calculadora/src/Calculadora.java
@@ -6,33 +6,24 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Calculadora {
-    public static void main(String[] args) {
-        //System.out.println(resolve("7/-7x-2/2"));
-        Arrays.stream("6x-6+-4/-2".split("-?\\d+")).forEach(a -> System.out.print(a+", "));
-        System.out.println();
-        Arrays.stream("6x-6+-4/-2".split("[+x/]")).forEach(a -> System.out.print(a+", "));
 
-
-
-    }
 
     public static String resolve(String operacion) {
-        if (operacion.startsWith("")) {
+        if (operacion.startsWith("-")) {
             operacion = new StringBuilder(operacion).insert(0, "0").toString();
         }
-        String [] dm = operacion.split("[+-]");
+        String[] dm = operacion.split("[+-]");
         List<String> dimu = new ArrayList<>(Arrays.asList(dm));
-        System.out.println(dimu);
         dimu = dimu.stream().map(o -> String.valueOf(resolveMultDiv(o))).collect(Collectors.toList());
 
-        String [] sr = operacion.split("[^\\+\\-]");
+        String[] sr = operacion.split("[^\\+\\-]");
         List<String> sure = new ArrayList<>(Arrays.asList(sr));
         sure.removeIf(String::isEmpty);
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < dimu.size(); i++) {
             sb.append(dimu.get(i));
-            if (i != dimu.size()-1) {
+            if (i != dimu.size() - 1) {
                 sb.append(sure.get(i));
             }
         }
@@ -42,7 +33,7 @@ public class Calculadora {
 
     public static double resolveMultDiv(String operation) {
         String[] auxNums = operation.split("[x/+-]");
-        String [] auxSimbs = operation.split("[0-9.]+");
+        String[] auxSimbs = operation.split("[0-9.]+");
 
         List<Double> nums = Arrays.stream(auxNums).map(Double::parseDouble).collect(Collectors.toList());
         List<String> simbs = new ArrayList<>(List.of(auxSimbs));
@@ -58,13 +49,13 @@ public class Calculadora {
                 double currentNum = nums.get(0);
                 double nxtNum = nums.get(1);
                 if (simbs.get(i).equals("x")) {
-                    result = currentNum*nxtNum;
+                    result = currentNum * nxtNum;
                 } else if (simbs.get(i).equals("/")) {
-                    result = currentNum/nxtNum;
+                    result = currentNum / nxtNum;
                 } else if (simbs.get(i).equals("+")) {
-                    result = currentNum+nxtNum;
+                    result = currentNum + nxtNum;
                 } else if (simbs.get(i).equals("-")) {
-                    result = currentNum-nxtNum;
+                    result = currentNum - nxtNum;
                 }
                 nums.remove(currentNum);
                 nums.remove(nxtNum);
@@ -87,8 +78,8 @@ public class Calculadora {
     }
 
     public static void resolver(String operacion) {
-        String [] ns = operacion.split("[/\\+\\-x]");
-        String [] ss = operacion.split("\\d+");
+        String[] ns = operacion.split("[/\\+\\-x]");
+        String[] ss = operacion.split("\\d+");
 
         List<String> nums = new ArrayList<>();
         List<String> signos = new ArrayList<>();
@@ -99,38 +90,37 @@ public class Calculadora {
         signos.remove(0);
 
 
-
         do {
             int i = signos.indexOf("x");
             int i2 = signos.indexOf("/");
             if (i != -1 && i2 != -1) {
                 int index = 0;
                 int r = 0;
-                if (i<i2) {
+                if (i < i2) {
                     index = i;
-                    r = Integer.parseInt(nums.get(index))*Integer.parseInt(nums.get(index+1));
-                } else if (i2<i) {
+                    r = Integer.parseInt(nums.get(index)) * Integer.parseInt(nums.get(index + 1));
+                } else if (i2 < i) {
                     index = i2;
-                    r = Integer.parseInt(nums.get(index))/Integer.parseInt(nums.get(index+1));
+                    r = Integer.parseInt(nums.get(index)) / Integer.parseInt(nums.get(index + 1));
 
                 }
-                signos.remove(index+1);
+                signos.remove(index + 1);
                 nums.set(index, String.valueOf(r));
-                nums.remove(index+1);
+                nums.remove(index + 1);
             } else {
-                for (int index = 0; index < nums.size()-1; index++) {
+                for (int index = 0; index < nums.size() - 1; index++) {
                     String s = signos.get(index);
                     int a = Integer.parseInt(nums.get(index));
-                    int b = Integer.parseInt(nums.get(index+1));
+                    int b = Integer.parseInt(nums.get(index + 1));
                     int r;
                     if (s.equals("+")) {
-                        r = a+b;
+                        r = a + b;
                     } else {
-                        r = a-b;
+                        r = a - b;
                     }
-                    signos.remove(index+1);
+                    signos.remove(index + 1);
                     nums.set(index, String.valueOf(r));
-                    nums.remove(index+1);
+                    nums.remove(index + 1);
                 }
                 break;
             }
