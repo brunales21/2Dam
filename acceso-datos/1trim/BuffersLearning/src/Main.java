378ca4f8ad5d4745c95034ebc7a641328542b4ce
unknown <bemimarket@gmail.com>
2023-10-20 13:10:03 +0200

    mas cosas

--- acceso-datos/1trim/BuffersLearning/src/Main.java
+++ acceso-datos/1trim/BuffersLearning/src/Main.java
@@ -6,11 +6,33 @@ public class Main {
     public static void main(String[] args) {
 
         List<JugadorFutbol> jugadores = new ArrayList<>();
-        String rutaFichero = "Pruebas/ejercicio1.dat";
+        String rutaFichero = "Pruebas/JugadoresFutbol.csv";
         EquipoFutbol equipo = new EquipoFutbol(jugadores);
         JugadorFutbol bruno = new JugadorFutbol("Bruno Mercado", "Argentina", 1, 1.0f, true);
 
-        /*
+        JugadorFutbol messi = new JugadorFutbol("Lionel Messi", "Argentina", 1, 0.75f, true);
+        JugadorFutbol maradona = new JugadorFutbol("Diego Maradona", "Argentina", 1, 0.5f, false);
+        JugadorFutbol cristiano = new JugadorFutbol("Cristiano Ronaldo", "Portugal", 0, 0.85f, true);
+        JugadorFutbol neymar = new JugadorFutbol("Neymar Jr.", "Brasil", 0, 0.6f, true);
+        JugadorFutbol ronaldinho = new JugadorFutbol("Ronaldinho", "Brasil", 1, 0.4f, false);
+        JugadorFutbol mbappe = new JugadorFutbol("Kylian Mbappé", "Francia", 1, 0.7f, true);
+
+        jugadores.add(bruno);
+        jugadores.add(messi);
+        jugadores.add(maradona);
+        jugadores.add(ronaldinho);
+        jugadores.add(cristiano);
+        jugadores.add(neymar);
+        jugadores.add(mbappe);
+
+        equipo.writeJugadoresToCsv(rutaFichero);
+        equipo.readCsv();
+
+        jugadores.forEach(a -> System.out.println(a));
+
+/*
+
+
         JugadorFutbol j1 = new JugadorFutbol();
         JugadorFutbol j2 = new JugadorFutbol();
         JugadorFutbol j3 = new JugadorFutbol();
@@ -28,23 +50,8 @@ public class Main {
         jugadores.add(j6);
         jugadores.add(j7);
 
-         */
 
 
-        JugadorFutbol messi = new JugadorFutbol("Lionel Messi", "Argentina", 1, 0.75f, true);
-        JugadorFutbol maradona = new JugadorFutbol("Diego Maradona", "Argentina", 1, 0.5f, false);
-        JugadorFutbol cristiano = new JugadorFutbol("Cristiano Ronaldo", "Portugal", 0, 0.85f, true);
-        JugadorFutbol neymar = new JugadorFutbol("Neymar Jr.", "Brasil", 0, 0.6f, true);
-        JugadorFutbol ronaldinho = new JugadorFutbol("Ronaldinho", "Brasil", 1, 0.4f, false);
-        JugadorFutbol mbappe = new JugadorFutbol("Kylian Mbappé", "Francia", 1, 0.7f, true);
-
-        jugadores.add(bruno);
-        jugadores.add(messi);
-        jugadores.add(maradona);
-        jugadores.add(ronaldinho);
-        jugadores.add(cristiano);
-        jugadores.add(neymar);
-        jugadores.add(mbappe);
 
 
         //EquipoFutbol.writeObjectsToBinaryFile(rutaFichero, equipo.getJugadores());
@@ -77,6 +84,7 @@ public class Main {
 
         //equipo.readCsv();
         //equipo.getJugadores().forEach(System.out::println);
+
 
 
 
